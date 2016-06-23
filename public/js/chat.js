2a2f1c839f058da44027937e7590e0570b051bee
y-kobayashi.t <y-kobayashi.t@voltage.co.jp>
2016-06-23 18:57:21 +0900

    [add] sound button to chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,5 @@
 app
-    .factory('ChatImage', function(FireBaseStorageService, $firebaseObject,File, $q) {
+    .factory('ChatImage', function(FireBaseStorageService, $firebaseObject,File,$filter, $q) {
         var _this = {};
         var chatRef = {};
 
@@ -34,6 +34,17 @@ app
             var d = $q.defer();
             File.readURL(file, d);
             return d.promise;
+        };
+
+        _this.getFileType = function(name){
+            var fileType = name.split('.').pop().split('?')[0];
+            if($filter('inArray')(['png','jpg', 'gif', 'bmp'], fileType)){
+                return 'image';
+            }else if($filter('inArray')(['mp3','wav', 'm4a'], fileType)){
+                return 'sound';
+            }else if($filter('inArray')(['wmv','mp4', 'mpeg', 'mov', 'avi'], fileType)){
+                return 'movie';
+            }
         };
 
         return _this;
@@ -128,7 +139,7 @@ app
                   _comments: [],
                   _refresh: function(data) {
                       this._comments = data.filter(function(el) {
-                          if(el.imageUrl) el.fileType = el.imageUrl.split('.').pop().split('?')[0];
+                          if(el.imageUrl && !el.fileType) el.fileType = ChatImage.getFileType(el.imageUrl);
                           return !angular.isDefined(el._excluded) || el._excluded === false;
                       });
                   },
@@ -143,6 +154,7 @@ app
                 $scope.dataset._refresh($scope.chat.comments);
                 $scope.chat.images = $filter('find')($scope.chat.comments,{imageUrl : true}, false);
                 $scope.imageDataset._refresh($scope.chat.images);
+                console.log($scope.imageDataset)
                 Vibration.play(500);
                 Loading.finish();
                 angular.element('.md-virtual-repeat-scroller').scrollTop(0);
@@ -158,10 +170,12 @@ app
                 };
                 if(imageUrl){
                     record.imageUrl = imageUrl;
+                    record.fileType = ChatImage.getFileType(imageUrl);
                 }else{
                     record.detail = $scope.comment;
                 }
-                $scope.comments = Chat.getComment($stateParams.value.$id, $scope.comment);
+                var id = $stateParams.value ? $stateParams.value.$id : $sessionStorage.toParams.value.$id;
+                $scope.comments = Chat.getComment(id, $scope.comment);
                 $scope.comments.$add(record).then(function(ref) {
                     var id = ref.key;
                     $scope.comments.$indexFor(id); // returns location in the array
@@ -178,17 +192,18 @@ app
             $scope.upload = function(){
                 if(!$scope.file) return;
                 Loading.start();
-                var fileType = $scope.file.name.split('.').pop().split('?')[0];
+                $scope.uploadFileType = ChatImage.getFileType($scope.file.name);
                 ChatImage.upload('chats', 'chats', $scope.file).then(function(updateImageUrl){
                     if(typeof updateImageUrl === 'string' || updateImageUrl instanceof String){
                         $scope.updateImageUrl = updateImageUrl;
                         $scope.addComment(updateImageUrl);
                     }
                 });
-                if($filter('inArray')(['png','jpg', 'gif'], fileType)){
+                if($scope.uploadFileType == 'image'){
                     ChatImage.getUploadFile($scope.file).then(function(uploadFileUrl){
                         $scope.uploadFileUrl = uploadFileUrl;
                     });
+                    return;
                 }
             };
         }
